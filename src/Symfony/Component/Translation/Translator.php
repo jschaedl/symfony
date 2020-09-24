88ac3ee53cc732e4c627dfbf9744a19413197677
Jan Schädlich <jan.schaedlich@sensiolabs.de>
2020-09-24 17:45:37 +0200

    Remove depreaction and fix getLocale

--- src/Symfony/Component/Translation/Translator.php
+++ src/Symfony/Component/Translation/Translator.php
@@ -158,7 +158,7 @@ class Translator implements TranslatorInterface, TranslatorBagInterface, LocaleA
      */
     public function getLocale()
     {
-        return $this->locale ?: \Locale::getDefault();
+        return $this->locale ?: (class_exists(\Locale::class) ? \Locale::getDefault() : 'en');
     }
 
     /**
